49d84884c0e5ed63dbdef6d3c45990d01bcafb33
manu <manu@localhost.localdomain>
2022-09-09 06:53:57 -0500

    HTB

---
+++ Active/content/decrypt.py
@@ -0,0 +1,7 @@
+encrypted = "edBSHOwhZLTjt/QS9FeIcJ83mjWA98gw9guKOhJOdcqh+ZGMeXOsQbCpZ3xUjTLfCuNH8pG5aSVYdYw/NglVmQ"
+encrypted = encrypted + "=" * (4 - (len(encrypted) % 4))
+encrypted = encrypted.decode(encoding='base64')
+key = "\x4e\x99\x06\xe8\xfc\xb6\x6c\xc9\xfa\xf4\x93\x10\x62\x0f\xfe\xe8\xf4\x96\xe8\x06\xcc\x05\x79\x90\x20\x9b\x09\xa4\x33\xb6\x6c\x1b"
+iv = "\x00" * 16
+aes = AES.new(key, AES.MODE_CBC, iv)
+print (aes.decrypt(encrypted).decode(encoding='ascii'))
